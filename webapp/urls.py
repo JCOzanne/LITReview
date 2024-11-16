c64d4a5e40355ec5547d53322b846a47369efd40
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2024-11-16 18:33:56 +0100

    settings et urls pour autentification

---
+++ webapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+import authentication.views
+import blog.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', authentication.views.login_page, name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', blog.views.home, name='home'),
+    path('signup/', authentication.views.signup_page, name='signup'),
+]
